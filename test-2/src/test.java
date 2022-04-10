6f9033b2045bf42f2dd3d4b2ecd287f5ea321ad7
Horfi <grudka@interia.pl>
2022-04-10 22:28:16 +0200

    Create test.java

---
+++ test-2/src/test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("podaj swoje imie: ");
+		String imie = scan.nextLine();
+		System.out.println(imie);
+	}
+
+}
